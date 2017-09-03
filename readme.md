76ab35678aeaae182bafc9dfdd3a5301625128ae
Rafael Machado <rafael.m.machado@hotmail.com>
2017-09-03 00:39:13 -0300

    Search Bar implementation

---
+++ readme.md
@@ -0,0 +1,3 @@
+Web React Film Library 
+
+Using React js to build a youtube dinamic video list
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import _ from 'lodash';
+
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -27,7 +30,12 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+
+        YTSearch({ key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
@@ -38,15 +46,16 @@ class App extends Component {
 
 
 
-
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
-                <VideoList 
-                onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-                videos={this.state.videos} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
             </div>
         );
     }
